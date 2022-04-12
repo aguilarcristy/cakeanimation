e08b306685588fb5a1d99bb56fe3ac7e1d37ca89
aguilarcristy <90861104+aguilarcristy@users.noreply.github.com>
2022-04-11 22:37:47 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# 3.1
+
+## Overview
+This is for assignment 3.1. The goal of this assignment is to familiarize myself with the libraries that p5.js offers.
+I would like to be able to use p5.play.
+
+### The repository for this project can be found
+[here](https://github.com/aguilarcristy/3.1)
+### Timeline:
+
+Project Started:
+Basic Outline by 04/?/2022
+Semi-Advanced Outline 04/?/2022
+Advanced/Final Outline by 04/?/2022
---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Creating animations
+
+//animations like p5 images should be stored in variables
+//in order to be displayed during the draw cycle
+var cake;
+
+//it's advisable (but not necessary) to load the images in the preload function
+//of your sketch otherwise they may appear with a little delay
+function preload() {
+
+  //create an animation from a sequence of numbered images
+  //pass the first and the last file name and it will try to find the ones in between
+  cake = loadAnimation('cake1.png','cake3.png','cake2.png','cake4.png','cake5.png','cake6.png','cake7.png','cake7.png');
+  
+}
+
+function setup() {
+  createCanvas(900, 900);
+}
+
+function draw() {
+  background(255, 247, 189);
+
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(cake, 450, 450);
+}
